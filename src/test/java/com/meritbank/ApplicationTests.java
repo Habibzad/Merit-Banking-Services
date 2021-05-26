d1da7b137c540adca8757f9b91375b4ce4671d60
Ahmad Habibzad <masoud.habibzad@gmail.com>
2021-05-26 14:45:26 -0400

    initial commit

---
+++ src/test/java/com/meritbank/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meritbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
